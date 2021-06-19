feccb58ac31733202608b8f5641fabeeae86b4e3
rares1928 <rares1928@gmail.com>
2021-06-20 02:24:23 +0300

    am uitat de footer

--- src/pagini/homePage.js
+++ src/pagini/homePage.js
@@ -8,21 +8,35 @@ import { makeStyles } from '@material-ui/core/styles';
 import HomeCard from '../componente/homeCard';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import IconButton from "@material-ui/core/IconButton";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
 
 
 const useStyles = makeStyles((theme)=>({
     root: {
-        paddingTop: theme.spacing(5),
-        paddingBottom: theme.spacing(5),
+        paddingTop: theme.spacing(6),
+        paddingBottom: theme.spacing(6),
     },
+    footer: {
+        backgroundColor: theme.palette.primary.main,
+        padding: theme.spacing(1,10,3),
+    },
+    footerItem: {
+        maxWidth: 300,
+    }
   }));
 
 export default function HomePage() {
     const classes=useStyles();
 
     return(
+    <>
         <Container className={classes.root} maxWidth="lg">
-            <Grid justify="center" container spacing={3} >
+            <Grid justify="center" container spacing={6} >
                 <Grid item>
                     <HomeCard
                         imagine={test}
@@ -69,5 +83,44 @@ export default function HomePage() {
                 
             </Grid>
         </Container>
+        <footer className={classes.footer}>
+            <Grid 
+                container
+                direction="row"
+                justify="space-between"
+                spacing={4}
+            >
+                <Grid className={classes.footerItem} item>
+                    <Typography variant="h6"  gutterBottom>
+                        Contact
+                    </Typography>
+                <Typography component="p">
+                    Pentru orice fel intrebări sau sugestii,
+                    vă rugam să ne contactați la adresa de mail:
+                    rezigo.contact@gmail.com
+                </Typography>
+                </Grid>
+                <Grid container direction="column" className={classes.footerItem} item>
+                    <Typography variant="h6"  gutterBottom>
+                        Link-uri utile
+                    </Typography>
+                    <Link color="secondary" href="/">Despre noi</Link>
+                    <Link color="secondary" href="/">Termeni și condiții</Link>
+                    <Link color="secondary" href="/">Întrebări frecvente</Link>
+                </Grid>
+                <Grid className={classes.footerItem} item>
+                    <Typography  variant="h6"  gutterBottom>
+                        Păstrează legătura
+                    </Typography>
+                    <IconButton href="/">
+                        <FacebookIcon color="secondary"  fontSize="large" />
+                    </IconButton>
+                    <IconButton href="/">
+                        <InstagramIcon color="secondary" fontSize="large" />
+                    </IconButton>
+                </Grid>
+            </Grid>
+        </footer>
+    </>
    );
 }
