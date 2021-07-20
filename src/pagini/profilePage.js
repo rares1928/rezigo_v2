906a694194856d53b83122907b5d684f08c4000c
rares1928 <rares1928@gmail.com>
2021-07-21 00:01:32 +0300

    mici modificar profile page

--- src/pagini/profilePage.js
+++ src/pagini/profilePage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
@@ -12,38 +12,59 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import EditIcon from '@material-ui/icons/Edit';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import InputBase from '@material-ui/core/InputBase';
+import { callApi } from '../utils/callApi';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 
-const useStyles = makeStyles((theme)=>({
-    root: {
-        paddingTop: theme.spacing(6),
-        paddingBottom: theme.spacing(6),
-        
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
-    divAccordion: {
-        padding: theme.spacing(1),
-    },
-    textField: {
-        padding: theme.spacing(1),
-    },
-    gridButton: {
-        margin: theme.spacing(1),
-    },
-}));
+export default function ProfilePage(props) {
 
-export default function ProfilePage() {
+    const useStyles = makeStyles((theme)=>({
+        root: {
+            paddingTop: theme.spacing(6),
+            paddingBottom: theme.spacing(6),
+            
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            paddingRight: theme.spacing(1),
+        },
+        secondaryHeading: {
+            fontSize: theme.typography.pxToRem(15),
+            color: theme.palette.text.secondary,
+        },
+        typographyHeader:{
+            padding: theme.spacing(1),
+        },
+        divAccordion: {
+            padding: theme.spacing(1),
+        },
+        accordion: {
+            backgroundColor: props.darkMode? "#5c5c5c" : "#fafafa",
+        },
+        textField: {
+            padding: theme.spacing(1),
+        },
+        gridButton: {
+            margin: theme.spacing(1),
+        },
+        logoutButtonDiv:{
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-end"
+        },
+    }));
+
     let history = useHistory();
     const classes=useStyles();
     const [name, setName] = useState('false');
     const [email, setEmail] = useState('false');
     const [password, setPassword] = useState('false');
     const [premium, setPremium] = useState('false');
+
+    const [ready, setReady] = useState(false);
+    const [items, setItems] = useState({});
+    const [error, setError] = useState(0);
 
     const [changeLastName, setChangeLastName] = useState('');
     const [changeFirstName, setChangeFirstName] = useState('');
@@ -57,6 +78,21 @@ export default function ProfilePage() {
     const [changeNewEmail, setChangeNewEmail] = useState('');
     const [verifyPasswordEmail, setVerifyPasswordEmail] = useState('');
 
+
+    const handleItems = (e) => {
+        setItems(e.data);
+    };
+
+    const handleError = (e) => {
+        setError(e);
+    }
+
+    useEffect( () => {
+        const url = "https://grileapiwin.azurewebsites.net/api/GetProfil?code=an7l2kCHdoYlNw006LoBdCzHB5U4qSVbNvpQ1r1V3TgSHtAYuMbkyw==";
+        callApi(url, {}, handleItems, handleError);
+        setReady(true);
+    }, [])
+
     const delogare = () => {
         const cookies = new Cookies();
         cookies.remove('estiLogat');
@@ -66,21 +102,22 @@ export default function ProfilePage() {
         cookies.remove('lastname');
         history.push('/login')
     }
-    
+    console.log(items["tip_profil"]);
     return(
         <Container className={classes.root} maxWidth="sm">
             <Paper>
-                <Typography variant="h2">Profil </Typography>
-                <Button variant="contained" color="secondary" onClick={() => delogare()}> Logout </Button>
+                <Typography className={classes.typographyHeader} variant="h5">Informații personale:</Typography>
+                {!ready? <CircularProgress/> :
                 <div className={classes.divAccordion}>
-                    <Accordion>
+                    <Accordion className={classes.accordion}>
                         <AccordionSummary
                             expandIcon={<EditIcon color='secondary'/> }
                             aria-controls="panel1a-content"
                             id="panel1a-header"
                             onClick={() => {setName(!name)}}
                         >
-                            <Typography className={classes.heading}>Nume</Typography>
+                            <Typography className={classes.heading}>Nume: </Typography>
+                            <Typography className={classes.secondaryHeading}> {items.lastName} {items.firstName} </Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Grid container direction="column">
@@ -96,13 +133,13 @@ export default function ProfilePage() {
                                 <Grid item >    
                                     <TextField 
                                     className={classes.textField} 
-                                    label="Nume" 
+                                    label={items.lastName}
                                     variant="outlined" 
                                     color="secondary" 
                                     onInput={e => setChangeLastName(e.target.value)}/>
                                     <TextField 
                                     className={classes.textField} 
-                                    label="Prenume" 
+                                    label={items.firstName} 
                                     variant="outlined" 
                                     color="secondary" 
                                     onInput={e => setChangeFirstName(e.target.value)}/>
@@ -120,7 +157,7 @@ export default function ProfilePage() {
                             </Grid>
                         </AccordionDetails>
                     </Accordion>
-                    <Accordion>
+                    <Accordion className={classes.accordion}>
                         <AccordionSummary
                             expandIcon={<EditIcon color='secondary'/> }
                             aria-controls="panel2a-content"
@@ -134,7 +171,7 @@ export default function ProfilePage() {
                                 <Grid item >
                                     <TextField 
                                     className={classes.textField} 
-                                    label="Parolă Veche" 
+                                    label="Parolă veche" 
                                     variant="outlined" 
                                     color="secondary" 
                                     type="password" 
@@ -143,14 +180,14 @@ export default function ProfilePage() {
                                 <Grid item >    
                                     <TextField 
                                     className={classes.textField} 
-                                    label="Parolă Nouă" 
+                                    label="Parolă nouă" 
                                     variant="outlined" 
                                     color="secondary" 
                                     type="password" 
                                     onInput={e => setChangeNewPassword(e.target.value)}/>
                                     <TextField 
                                     className={classes.textField} 
-                                    label="Reintroducere Parolă" 
+                                    label="Reintrodu parola nouă" 
                                     variant="outlined" 
                                     color="secondary" 
                                     type="password" 
@@ -168,7 +205,7 @@ export default function ProfilePage() {
                             </Grid>
                         </AccordionDetails>
                     </Accordion>
-                    <Accordion>
+                    <Accordion className={classes.accordion}>
                         <AccordionSummary
                             expandIcon={<EditIcon color='secondary'/> }
                             aria-controls="panel2a-content"
@@ -176,6 +213,7 @@ export default function ProfilePage() {
                             onClick={() => {setName(!password)}}
                         >
                             <Typography className={classes.heading}>Email</Typography>
+                            <Typography className={classes.secondaryHeading}> {items.email} </Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Grid container direction="column">
@@ -185,18 +223,13 @@ export default function ProfilePage() {
                                     label="Parolă" 
                                     variant="outlined" 
                                     color="secondary" 
+                                    type="password"
                                     onInput={e => setVerifyPasswordEmail(e.target.value)}/>
                                 </Grid>
                                 <Grid item >    
                                     <TextField 
                                     className={classes.textField} 
-                                    label="Email Vechi" 
-                                    variant="outlined" 
-                                    color="secondary" 
-                                    onInput={e => setChangeOldEmail(e.target.value)}/>
-                                    <TextField 
-                                    className={classes.textField} 
-                                    label="Email Nou" 
+                                    label="Email nou" 
                                     variant="outlined" 
                                     color="secondary" 
                                     onInput={e => setChangeNewEmail(e.target.value)}/>
@@ -212,32 +245,24 @@ export default function ProfilePage() {
                             </Grid>
                         </AccordionDetails>
                     </Accordion>
-                    <Accordion>
+                    <Accordion className={classes.accordion}>
                     <AccordionSummary
-                    expandIcon={<EditIcon color='secondary'/> }
+                    disabled={!(items["tip_profil"] === "Premium")}
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     onClick={() => {setName(!premium)}}
                     >
                         <Typography className={classes.heading}>Tip Cont</Typography>
+                        <Typography className={classes.secondaryHeading}>{items["tip_profil"]}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Grid container direction="row">
-                            {premium ?
-                                <InputBase
-                                    defaultValue="Premium"
-                                    inputProps={{ 'aria-label': 'naked' }}
-                                /> :
-                                <InputBase
-                                    defaultValue="Non-Premium"
-                                    inputProps={{ 'aria-label': 'naked' }}
-                                />
+                            {(items["tip_profil"] === "Premium") ?
+
+                                <Typography> Valabil până la: {items.createdAt} </Typography> :
+                                null
                             }
-                            <InputBase
-                                defaultValue="25.05.2022"
-                                inputProps={{ 'aria-label': 'naked' }}
-                            />
-                            {!premium ? 
+                            {!(items["tip_profil"] === "Premium") ?
                             <Grid className={classes.gridButton} item>
                                     <Button variant="contained" color="secondary">Cumpără Premium</Button>
                             </Grid> : null
@@ -245,6 +270,10 @@ export default function ProfilePage() {
                         </Grid>
                     </AccordionDetails>
                 </Accordion>
+                </div>
+                }
+                <div className={classes.logoutButtonDiv}>
+                <Button className={classes.gridButton} variant="contained" color="secondary" onClick={() => delogare()}> Delogare </Button>
                 </div>
             </Paper>
         </Container>
