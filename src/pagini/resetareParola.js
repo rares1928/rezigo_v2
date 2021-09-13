f9390aaa755d0adb7b5f8d15688213de18361c4b
rares1928 <rares1928@gmail.com>
2021-09-13 22:45:15 +0300

    ai uitat parola2

--- src/pagini/resetareParola.js
+++ src/pagini/resetareParola.js
@@ -3,7 +3,6 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import { useLocation } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid';
 import logo from '../poze/logo4.svg';
 import Typography from '@material-ui/core/Typography';
