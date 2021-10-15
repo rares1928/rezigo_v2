47c63f21bc21d9d69b5fd30b1d9a23695db28737
mateimanea <56696113+mateimanea@users.noreply.github.com>
2021-10-15 18:48:37 +0300

    /librarie

--- src/pagini/librariePage.js
+++ src/pagini/librariePage.js
@@ -2,13 +2,26 @@ import React, {useState} from 'react';
 import { Typography } from '@material-ui/core';
 import { Collapse } from '@material-ui/core';
 import { Paper } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { Container } from '@material-ui/core';
+import { Card } from '@material-ui/core';
+import { CardActions } from '@material-ui/core';
 import { CardActionArea } from '@material-ui/core';
+import { CardContent } from '@material-ui/core';
+import {Slide} from '@material-ui/core';
 import { FormControlLabel } from '@material-ui/core';
 import { Switch } from '@material-ui/core';
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+    card:{
+        display: "flex",
+        flexDirection: "row",
+        marginBottom: theme.spacing(0.5),
+        width: "100%",
+        height: '15vh'
+    },
     wrapper:{
         // height: "80vh",
         // width: "100%",
@@ -18,25 +31,52 @@ const useStyles = makeStyles((theme) => ({
         alignItems: "start",
         backgroundColor: "black",
     },
+    paper: {
+        padding: theme.spacing(2),
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(0),
+    },
     cardFront: {
-        position: "absolute",
+        // position: "absolute",
         zIndex: "1",
+        backgroundColor:"#B1EEBE"
     }, 
     cardBack: {
-        zIndex: "-1",
-        position: "absolute",
+        zIndex: "1",
+        // position: "absolute",
+        backgroundColor:'red'
     },  
     parentCard: {
         position: "relative",
         left: "10vw",
         marginTop: "2%"
     },
+    intrebareGrid: {
+        width: "100%",
+    },
+    hintGrid: {
+        minWidth: "10%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: "pointer",
+        marginRight: "10px",
+        // backgroundColor: "#DDEEF8"
+    },
+    raspunsHintGrid: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        // backgroundColor:"red"
+    }
 }));
 
 export default function LibrariePage() {
   
     const classes=useStyles();
-    
+
+
     const [checked, setChecked] = useState([true, true, true, true]);
 
     const handleChange = (index) => {
@@ -46,193 +86,70 @@ export default function LibrariePage() {
     };
 
     return (
-    <div className={classes.wrapper}>
-        {/* <Typography variant="h2">Librarie</Typography> */}
-        <Box className={classes.parentCard}>
-        {/* <FormControlLabel
-            control={<Switch checked={checked} onChange={handleChange} />}
-            label="Show"
-        /> */}
-        <Collapse
-            className={classes.cardFront}
-            in={checked[0]}
-            collapsedSize={0}
-            orientation="horizontal"
-        >
-            <Box
-            onClick={() => handleChange(0)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "primary.light"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-                Doi pereti vorbesc:
-            </Typography>
-            </Box>
-        </Collapse>
-        {/* <Collapse
-            className={classes.cardBack}
-            in={!checked}
-            collapsedSize={0}
-            orientation="vertical"
-        > */}
-            <Box
-            onClick={() => handleChange(0)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "secondary.dark"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-                Ne-ntalnim la colt.
-            </Typography>
-            </Box>
-        {/* </Collapse> */}
-        </Box>
-        <Box className={classes.parentCard}>
-        {/* <FormControlLabel
-            control={<Switch checked={checked} onChange={handleChange} />}
-            label="Show"
-        /> */}
-        <Collapse
-            className={classes.cardFront}
-            in={checked[1]}
-            collapsedSize={0}
-            orientation="vertical"
-        >
-            <Box
-            onClick={() => handleChange(1)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "primary.light"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-            - Care e diferenta dintre o gaina?
-            </Typography>
-            </Box>
-        </Collapse>
-        {/* <Collapse
-            className={classes.cardBack}
-            in={!checked}
-            collapsedSize={0}
-            orientation="vertical"
-        > */}
-            <Box
-            onClick={() => handleChange(1)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "secondary.dark"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-            - Are picioarele paralele, mai ales stangul.
-            </Typography>
-            </Box>
-            
-        {/* </Collapse> */}
-        </Box>
-        <Box className={classes.parentCard}>
-        {/* <FormControlLabel
-            control={<Switch checked={checked} onChange={handleChange} />}
-            label="Show"
-        /> */}
-        <Collapse
-            className={classes.cardFront}
-            in={checked[2]}
-            collapsedSize={0}
-            orientation="vertical"
-        >
-            <Box
-            onClick={() => handleChange(2)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "primary.light"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-            Doi crocodili zboara peste atlantic
-            </Typography>
-            </Box>
-        </Collapse>
-        {/* <Collapse
-            className={classes.cardBack}
-            in={!checked}
-            collapsedSize={0}
-            orientation="vertical"
-        > */}
-            <Box
-            onClick={() => handleChange(2)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "secondary.dark"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-            Unu-i verde, altul o ia la stanga
-            </Typography>
-            </Box>
-        {/* </Collapse> */}
-        </Box>
-        <Box className={classes.parentCard}>
-        {/* <FormControlLabel
-            control={<Switch checked={checked} onChange={handleChange} />}
-            label="Show"
-        /> */}
-        <Collapse
-            className={classes.cardFront}
-            in={checked[3]}
-            collapsedSize={0}
-            orientation="vertical"
-        >
-            <Box
-            onClick={() => handleChange(3)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "primary.light"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-                Foaie vede barabula, apa-mi vine pan-la
-            </Typography>
-            </Box>
-        </Collapse>
-        {/* <Collapse
-            className={classes.cardBack}
-            in={!checked}
-            collapsedSize={0}
-            orientation="vertical"
-        > */}
-            <Box
-            onClick={() => handleChange(3)}
-            sx={{
-                width: 600,
-                height: 50,
-                borderRadius: "16px",
-                bgcolor: "secondary.dark"
-            }}
-            >
-            <Typography gutterBottom align="center" variant="h6">
-                ...brau
-            </Typography>
-            </Box>
-        {/* </Collapse> */}
-        </Box>
-    </div>
+    <Container maxWidth="lg"> 
+        <Grid container direction="row" sx={{bgcolor:'red', display:'flex', direction:'row'}}>
+            <Grid className={classes.intrebareGrid} item>
+                <Paper className={classes.paper} >
+                    <Card className={classes.card} sx={{height:'40vh'}}>
+                        <CardContent className={classes.hintGrid}>
+                            <Typography align="center" variant="body2">
+                                Nr. Grila:
+                            </Typography>
+                        </CardContent>
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography variant="body2">
+                                    Doi pereti vorbesc
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Paper>
+            </Grid>
+        </Grid>
+        <Grid contianer direction="column">
+            <Grid item>
+                <Paper className={classes.paper}>
+                    <Card className={classes.card}>
+                        <CardActions className={classes.hintGrid} onClick={() => handleChange(0)}>
+                            <Typography variant="body2" color="textPrimary">
+                                Hint:
+                            </Typography>
+                        </CardActions>
+                        {!checked[0] ? 
+                        <CardContent className={classes.raspunsHintGrid}>
+                            <Typography>Raspuns hint</Typography>
+                        </CardContent>    
+                            : null}
+                        <Slide direction="left" in={checked[0]}>
+                            <CardActionArea className={classes.cardFront}>
+                                <CardContent>
+                                    
+                                </CardContent>
+                            </CardActionArea>
+                        </Slide>
+                    </Card>
+                    <Card className={classes.card}>
+                        <CardActions className={classes.hintGrid} onClick={() => handleChange(1)}>
+                            <Typography variant="body2">
+                                Raspuns:
+                            </Typography>
+                        </CardActions>
+                        {!checked[1] ? 
+                        <CardContent className={classes.raspunsHintGrid}>
+                            <Typography>Actual raspuns</Typography>
+                        </CardContent>    
+                            : null}
+                        <Slide direction="left" in={checked[1]}>
+                            <CardActionArea className={classes.cardFront}>
+                                <CardContent>                            
+                                </CardContent>
+                            </CardActionArea>
+                        </Slide>
+                    </Card>
+                </Paper>
+            </Grid>
+        </Grid>
+    </Container>
     );
     }
