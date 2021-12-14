4b9dd80e400073675995b0d8c802c59efc19f039
rares1928 <rares1928@gmail.com>
2021-12-15 00:46:30 +0200

    merge editare grila

--- src/pagini/adminsEditGrila.js
+++ src/pagini/adminsEditGrila.js
@@ -5,15 +5,11 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import Divider from '@material-ui/core/Divider';
+import CircularProgress  from '@material-ui/core/CircularProgress';
 import TextField from '@material-ui/core/TextField';
 import { callApi } from '../utils/callApi';
 import { useLocation, useHistory } from 'react-router-dom';
-import { CircularProgress, Divider } from '@material-ui/core';
-
 
 const useStyles = makeStyles((theme)=>({
     wrapperDiv:{
@@ -49,7 +45,7 @@ const useStyles = makeStyles((theme)=>({
         padding: theme.spacing(1),
     },
     textFieldDiv: {
-        width: "95%",
+        width: "85%",
         margin: theme.spacing(1),
     },
     typography:{
@@ -77,14 +73,29 @@ const useStyles = makeStyles((theme)=>({
         marginBottom: theme.spacing(1),
         marginLeft: theme.spacing(1),
     },
-  }));
-
+    reportsDiv:{
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(2),
+    },
+    }));
 
 export default function AdminsEditGrila() {
+
     const [loading, setLoading] = useState(false);
     const [items, setItems] = useState({});
-    const [itemsModificate, setItemsModificate] = useState({});
     const [ready, setReady] = useState(false);
+    const [tipGrila, setTipGrila] = useState("");
+    const [intrebare, setIntrebare] = useState("");
+    const [varA, setVarA] = useState("");
+    const [varB, setVarB] = useState("");
+    const [varC, setVarC] = useState("");
+    const [varD, setVarD] = useState("");
+    const [varE, setVarE] = useState("");
+    const [raspA, setRaspA] = useState("");
+    const [raspB, setRaspB] = useState("");
+    const [raspC, setRaspC] = useState("");
+    const [raspD, setRaspD] = useState("");
+    const [raspE, setRaspE] = useState("");
     const { state } = useLocation();
     let history = useHistory();
 
@@ -104,19 +115,46 @@ export default function AdminsEditGrila() {
         else{
             console.log(e);
         }
-        setLoading(false);
+        setReady(false);
     }
     const handleItems = (e) => {
         setItems(e.data);
-        setItemsModificate(e.data);
+        setTipGrila(e.data.lista["TipGrile"]);
+        setIntrebare(e.data.lista["Intrebare"]);
+        setVarA(e.data.lista["Variante"][0]);
+        setVarB(e.data.lista["Variante"][1]);
+        setVarC(e.data.lista["Variante"][2]);
+        setVarD(e.data.lista["Variante"][3]);
+        setVarE(e.data.lista["Variante"][4]);
+        setRaspA(e.data.lista["Raspunsuri"][0]);
+        setRaspB(e.data.lista["Raspunsuri"][1]);
+        setRaspC(e.data.lista["Raspunsuri"][2]);
+        setRaspD(e.data.lista["Raspunsuri"][3]);
+        setRaspE(e.data.lista["Raspunsuri"][4]);
         sleep(100).then(() => setReady(true));
+    }
+
+    const updateGrila = () => {
+        setLoading(true);
+        const url = "https://grileapiwin.azurewebsites.net/api/EditeazaGrila?code=Mik8i81eKx/6UL8G21W3LRpNSgm2naWAT07Ub4FB7rQreyHgm/OkVQ==";
+        const data = {
+            GrilaId: state,
+            Raspunsuri: [raspA, raspB, raspC, raspD, raspE],
+            Raspunsuri_numar: 16 * raspA + 8 *raspB + 4 * raspC + 2*raspD + raspE,
+            TipGrile: tipGrila,
+            Variante: [varA, varB, varC, varD, varE],
+            Explicatii: items.lista["Explicatii"]
+        }
+        callApi(url, data, succes, handleError);
+    }
+    const succes = () => {
+        window.location.reload();
     }
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    console.log(items.lista);
     const classes=useStyles();
     const TITLE = "admins";
     return(
@@ -134,14 +172,20 @@ export default function AdminsEditGrila() {
                     <Typography >
                         Id grila: {state}
                     </Typography>
+                    <Typography >
+                        Autor: {items.lista["Autor"]}
+                    </Typography>
+                    <Typography >
+                        Carte:{items.lista["Carte"]} ; Capitol: {items.lista["Categorie"]} ; Sub capitol: {items.lista["SubCategorie"]}
+                    </Typography>
                     <div className={classes.tipGrileDiv}>
                         <Typography>
                             Tip Grila:
                         </Typography>
-                        <Button variant='contained' color={items.lista["TipGrile"] === "CS" ? "secondary" : ""}>
+                        <Button disableElevation variant='contained' color={items.lista["TipGrile"] === "CS" ? "secondary" : "inherit"}>
                             CS
                         </Button>
-                        <Button variant='contained' color={items.lista["TipGrile"] === "CM" ? "secondary" : ""}>
+                        <Button disableElevation variant='contained' color={items.lista["TipGrile"] === "CM" ? "secondary" : "inherit"}>
                             CM
                         </Button>
                     </div>
@@ -164,7 +208,7 @@ export default function AdminsEditGrila() {
                         e){items.lista["Variante"][4]}
                     </Typography>
                     <Typography >
-                        Explicatii: {/* e) {grilaPrimita.Variante_e} */}
+                        Explicatii: {items.lista["Explicatii"]}
                     </Typography>
                     <Typography >
                         Raspunsuri numar: {items.lista["Raspunsuri_numar"]}
@@ -173,19 +217,19 @@ export default function AdminsEditGrila() {
                         <Typography>
                             Raspunsuri corecte:
                         </Typography>
-                        <Button variant='contained' color={items.lista["Raspunsuri"][0] === 1 ? "secondary" : ""}>
+                        <Button disableElevation variant='contained' color={items.lista["Raspunsuri"][0] === 1 ? "secondary" : "inherit"}>
                             a)
                         </Button>
-                        <Button variant='contained' color={items.lista["Raspunsuri"][1] === 1 ? "secondary" : ""}>
+                        <Button disableElevation variant='contained' color={items.lista["Raspunsuri"][1] === 1 ? "secondary" : "inherit"}>
                             b)
                         </Button>
-                        <Button variant='contained' color={items.lista["Raspunsuri"][2] === 1 ? "secondary" : ""}>
+                        <Button disableElevation variant='contained' color={items.lista["Raspunsuri"][2] === 1 ? "secondary" : "inherit"}>
                             c)
                         </Button>
-                        <Button variant='contained' color={items.lista["Raspunsuri"][3] === 1 ? "secondary" : ""}>
+                        <Button disableElevation variant='contained' color={items.lista["Raspunsuri"][3] === 1 ? "secondary" : "inherit"}>
                             d)
                         </Button>
-                        <Button variant='contained' color={items.lista["Raspunsuri"][4] === 1 ? "secondary" : ""}>
+                        <Button disableElevation variant='contained' color={items.lista["Raspunsuri"][4] === 1 ? "secondary" : "inherit"}>
                             e)
                         </Button>
                     </div>
@@ -197,52 +241,169 @@ export default function AdminsEditGrila() {
                     <Typography >
                         Id grila: {state}
                     </Typography>
+                    <Typography >
+                        Autor: {items.lista["Autor"]}
+                    </Typography>
+                    <Typography >
+                        Carte:{items.lista["Carte"]} ; Capitol: {items.lista["Categorie"]} ; Sub capitol: {items.lista["SubCategorie"]}
+                    </Typography>
                     <div className={classes.tipGrileDiv}>
                         <Typography>
                             Tip Grila:
                         </Typography>
-                        <Button variant='contained' color={itemsModificate.lista["TipGrile"] === "CS" ? "secondary" : ""}>
+                        <Button 
+                        disableElevation
+                        variant="contained"
+                        color={tipGrila === "CS" ? "secondary" : "inherit"}
+                        onClick={() => {setTipGrila("CS")}}
+                        >
                             CS
                         </Button>
-                        <Button variant='contained' color={itemsModificate.lista["TipGrile"] === "CM" ? "secondary" : ""}>
+                        <Button 
+                        disableElevation
+                        variant="contained"
+                        color={tipGrila === "CM" ? "secondary" : "inherit"}
+                        onClick={() => {setTipGrila("CM")}}
+                        >
                             CM
                         </Button>
                     </div>
-                    <Typography >
-                        
-                    </Typography>
-                    <Typography >
-                        a){/* a) {grilaPrimita.Variante_a} */}
-                    </Typography>
-                    <Typography >
-                        b){/* b) {grilaPrimita.Variante_b} */}
-                    </Typography>
-                    <Typography >
-                        c){/* c) {grilaPrimita.Variante_c} */}
-                    </Typography>
-                    <Typography >
-                        d){/* d) {grilaPrimita.Variante_d} */}
-                    </Typography>
-                    <Typography >
-                        e){/* e) {grilaPrimita.Variante_e} */}
+                    <div className={classes.textFieldDiv}> 
+                        <TextField
+                            color="secondary"
+                            name="Intrebare"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="Intrebare"
+                            label="Intrebare"
+                            autoFocus
+                            value={intrebare}
+                            onInput={e => setIntrebare(e.target.value)}
+                            multiline
+                                
+                        />
+                    </div>
+                    <div className={classes.textFieldDiv}> 
+                        <TextField
+                            color="secondary"
+                            name="varA"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="varA"
+                            label="a)"
+                            autoFocus
+                            value={varA}
+                            onInput={e => setVarA(e.target.value)}
+                            multiline
+                                
+                        />
+                    </div>
+                    <div className={classes.textFieldDiv}> 
+                        <TextField
+                            color="secondary"
+                            name="varB"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="varB"
+                            label="b)"
+                            autoFocus
+                            value={varB}
+                            onInput={e => setVarB(e.target.value)}
+                            multiline
+                                
+                        />
+                    </div>
+                    <div className={classes.textFieldDiv}> 
+                        <TextField
+                            color="secondary"
+                            name="varC"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="varC"
+                            label="c)"
+                            autoFocus
+                            value={varC}
+                            onInput={e => setVarC(e.target.value)}
+                            multiline
+                                
+                        />
+                    </div>
+                    <div className={classes.textFieldDiv}> 
+                        <TextField
+                            color="secondary"
+                            name="varD"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="varD"
+                            label="d)"
+                            autoFocus
+                            value={varD}
+                            onInput={e => setVarD(e.target.value)}
+                            multiline
+                                
+                        />
+                    </div>
+                    <div className={classes.textFieldDiv}> 
+                        <TextField
+                            color="secondary"
+                            name="varE"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="varE"
+                            label="e)"
+                            autoFocus
+                            value={varE}
+                            onInput={e => setVarE(e.target.value)}
+                            multiline
+                                
+                        />
+                    </div>
+                    <Typography>
+                        Raspunsuri numar: {16 * raspA + 8 *raspB + 4 * raspC + 2*raspD + raspE}
                     </Typography>
                     <div className={classes.raspunsuriDiv}>
                         <Typography>
                             Raspunsuri corecte:
                         </Typography>
-                        <Button variant='contained' disabled>
+                        <Button 
+                        variant='contained' 
+                        color={raspA === 1 ? "secondary" : "inherit"}
+                        onClick={() => setRaspA(raspA === 1 ? 0: 1)}
+                        >
                             a)
                         </Button>
-                        <Button variant='contained' disabled>
+                        <Button 
+                        variant='contained' 
+                        color={raspB === 1 ? "secondary" : "inherit"}
+                        onClick={() => setRaspB(raspB === 1 ? 0: 1)}
+                        >
                             b)
                         </Button>
-                        <Button variant='contained' disabled>
+                        <Button 
+                        variant='contained' 
+                        color={raspC === 1 ? "secondary" : "inherit"}
+                        onClick={() => setRaspC(raspC === 1 ? 0: 1)}
+                        >
                             c)
                         </Button>
-                        <Button variant='contained' disabled>
+                        <Button 
+                        variant='contained' 
+                        color={raspD === 1 ? "secondary" : "inherit"}
+                        onClick={() => setRaspD(raspD === 1 ? 0: 1)}
+                        >
                             d)
                         </Button>
-                        <Button variant='contained' disabled>
+                        <Button 
+                        variant='contained' 
+                        color={raspE === 1 ? "secondary" : "inherit"}
+                        onClick={() => setRaspE(raspE === 1 ? 0: 1)}
+                        >
                             e)
                         </Button>
                     </div>
@@ -252,7 +413,7 @@ export default function AdminsEditGrila() {
                         variant="contained" 
                         color="secondary" 
                         className={classes.button}
-                        
+                        onClick={updateGrila}
                         >
                             Update grila
                         </Button>
@@ -262,9 +423,37 @@ export default function AdminsEditGrila() {
                    Complaints:
                 </Typography>
                 <Paper className={classes.paper}>
-                    aici vor veni reports-urile
+                    {items.lista["Reports"].length === 0? <Typography>Nu exista niciun report</Typography>: 
+                        <div>
+                            {items.lista["Reports"].map((report, index) => 
+                            <div key={"report_numar_"+ String(index)} className={classes.reportsDiv}>
+                                <Typography>
+                                    Creat la: {report["CreatedOn"]}
+                                </Typography>
+                                <Typography>
+                                    Email: {report["Email"]}
+                                </Typography>
+                                <Typography>
+                                    Complaint: {report["Complaint"]}
+                                </Typography>
+                                <div className={classes.divButton}>
+                                    <div/>
+                                    <Button 
+                                    variant="contained" 
+                                    color="secondary" 
+                                    className={classes.button}
+                                    disabled
+                                    >
+                                        Sterge report
+                                    </Button>
+                                </div>
+                                <Divider/>
+                            </div>
+                            )}
+                        </div>
+                    }
                 </Paper>
-                </> : null
+                </> : <CircularProgress/>
             }
         </Container>
     </div>
