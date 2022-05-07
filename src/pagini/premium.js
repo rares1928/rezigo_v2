28d450bcc656501cefedfc9120e8fd1abf52f60c
rares1928 <rares1928@gmail.com>
2022-05-07 20:47:24 +0300

    update prices

--- src/pagini/premium.js
+++ src/pagini/premium.js
@@ -77,7 +77,7 @@ export default function Pricing() {
   const [loadCumpara, setLoadCumpara] = useState(false);
   const [items, setItems] = useState({});
   const [prices, setPrices] = useState([]);
-  const inversReducere = [3, 3, 3];
+  const inversReducere = [2, 2, 2];
 
   const handleError = (e) => {
     console.log(e.status);
