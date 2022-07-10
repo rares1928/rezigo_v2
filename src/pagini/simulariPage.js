95d9f76569a7ab340756c60388c41e2549e61e22
rares1928 <rares1928@gmail.com>
2022-07-11 00:04:50 +0300

    add continue button

--- src/pagini/simulariPage.js
+++ src/pagini/simulariPage.js
@@ -7,6 +7,7 @@ import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Link from "@material-ui/core/Link";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   wrapperDiv: {
@@ -52,8 +53,11 @@ const useStyles = makeStyles((theme) => ({
 export default function SimulariPage() {
   const classes = useStyles();
   const TITLE = "Simulări";
+  let history = useHistory();
+
   const [simulari, setSimulari] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [goLoading, setGoLoading] = useState(false);
 
   const handleSetSimulari = (e) => {
     setSimulari(e.data["lista"]);
@@ -68,16 +72,37 @@ export default function SimulariPage() {
 
   //New attempt
   const getSimulari = () => {
+    setLoading(true);
     const getSimulariFromDB = async () => {
       const url =
         "https://grileapiwin.azurewebsites.net/api/GetAllSimulariUser?code=HxAlZyvuHUfuO5kSUCBrxvOw_-HCrDXCa5CjQZrxW3QpAzFuSGekHQ==";
       const data = {};
-      await callApi(url, data, handleSetSimulari, handleError);
+      await callApi(url, data, handleSetSimulari, handleError).then(() => {
+        setLoading(false);
+      });
     };
     getSimulariFromDB();
   };
 
+  console.log(simulari);
   useEffect(getSimulari, []);
+
+  const handleTestSimulareId = (testSimulareId) => {
+    return history.push({
+      pathname: "/rezolva_test",
+      state: { testId: testSimulareId.data["lista"], testType: "simulare" },
+    });
+  };
+
+  const creeazaTestSimulare = async (simulareId) => {
+    setLoading(true);
+    let url =
+      "https://grileapiwin.azurewebsites.net/api/CreateTestFromSimulare?code=Rvw9qSqNw8iOf2W8PxysnQocQX7MPUoDFN8mdWnNgsyuAzFupH91ZA==";
+    const data = { simulareId: simulareId };
+    await callApi(url, data, handleTestSimulareId, handleError).then(() => {
+      setLoading(false);
+    });
+  };
 
   return (
     <div className={classes.wrapperDiv}>
@@ -89,63 +114,75 @@ export default function SimulariPage() {
         Simulări oficiale Rezigo
       </Typography>
 
-      {simulari.length === 0 ? (
-        <div>Momentan nu avem nicio simulare salvată</div>
+      {loading ? (
+        <CircularProgress />
       ) : (
-        <Paper className={classes.paper}>
-          <div>
-            {simulari.map((simulare, index) => (
-              <div
-                className={classes.paper}
-                key={"_test tip simulare:" + String(index)}
-              >
-                <div>
-                  <Typography className={classes.headerText} variant="h6">
-                    Nume: {simulare.Simulare.Name}
-                  </Typography>
-                  <div className={classes.simulareDiv}>
+        <>
+          {simulari.length === 0 ? (
+            <div>Momentan nu avem nicio simulare salvată</div>
+          ) : (
+            <Paper className={classes.paper}>
+              <div>
+                {simulari.map((simulare, index) => (
+                  <div
+                    className={classes.paper}
+                    key={"_test tip simulare:" + String(index)}
+                  >
                     <div>
-                      <Typography>
-                        Data la care începe:{" "}
-                        {simulare.Simulare.StartDate.split("T")[0]} ora{" "}
-                        {simulare.Simulare.StartDate.split("T")[1]}{" "}
+                      <Typography className={classes.headerText} variant="h6">
+                        Nume: {simulare.Simulare.Name}
                       </Typography>
-                      <Typography>
-                        Descriere: {simulare.Simulare.Description}
-                      </Typography>
-                    </div>
-                    <div>
-                      <Button
-                        size="medium"
-                        className={classes.buttons}
-                        variant="contained"
-                        color="secondary"
-                        onClick={() => {}}
-                        disabled={!simulare.APlatit}
-                      >
-                        {simulare.InceputTest === "true"
-                          ? "Continuă simularea"
-                          : "Începe simularea"}
-                      </Button>
+                      <div className={classes.simulareDiv}>
+                        <div>
+                          <Typography>
+                            Data la care începe:{" "}
+                            {simulare.Simulare.StartDate.split("T")[0]} ora{" "}
+                            {simulare.Simulare.StartDate.split("T")[1]}{" "}
+                          </Typography>
+                          <Typography>
+                            Descriere: {simulare.Simulare.Description}
+                          </Typography>
+                        </div>
+                        <div>
+                          <Button
+                            size="medium"
+                            className={classes.buttons}
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => {
+                              creeazaTestSimulare(simulare.Simulare.ID);
+                            }}
+                            disabled={!simulare.APlatit || goLoading}
+                          >
+                            {goLoading ? (
+                              <CircularProgress />
+                            ) : simulare.InceputTest === "true" ? (
+                              "Continuă simularea"
+                            ) : (
+                              "Începe simularea"
+                            )}
+                          </Button>
+                        </div>
+                      </div>
+                      {simulare.APlatit ? (
+                        ""
+                      ) : (
+                        <Typography className={classes.textNeplatit}>
+                          Din păcate nu ai acces la această simulare. O poți
+                          cumpăra de{" "}
+                          <Link href="/premium" color="secondary">
+                            {" "}
+                            aici
+                          </Link>
+                        </Typography>
+                      )}
                     </div>
                   </div>
-                  {simulare.APlatit ? (
-                    ""
-                  ) : (
-                    <Typography className={classes.textNeplatit}>
-                      Din păcate nu ai acces la această simulare. O poți cumpăra
-                      de{" "}
-                      <Link href="/premium" color="secondary">
-                        {" "}
-                        aici
-                      </Link>
-                    </Typography>
-                  )}
-                </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </Paper>
+            </Paper>
+          )}
+        </>
       )}
     </div>
   );
