be56ed57d1e6b49a8c11eaf71ac8167a57474637
Agodak <dragos_gegea@yahoo.com>
2022-10-19 22:56:32 +0100

    Link nu are disable, change href

--- src/pagini/simulariPage.js
+++ src/pagini/simulariPage.js
@@ -250,7 +250,7 @@ export default function SimulariPage() {
                                                     </Button>
                                                 </div>
                                                 <div className={classes.centerDiv}>
-                                                    <Link color="secondary" href={rezultate} download className={classes.centerDiv} disabled={new Date(simulare.Simulare.StartDate).getTime() > Date.now()}>
+                                                    <Link color="secondary" href={rezultate ? new Date(simulare.Simulare.StartDate).getTime() < Date.now() : "#"} download className={classes.centerDiv}>
                                                         Rezultate simulare
                                                     </Link>
                                                 </div>
