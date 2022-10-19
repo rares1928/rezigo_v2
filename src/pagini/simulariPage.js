75c815c843b67ff05078abecccd471738e1b3f3f
Agodak <dragos_gegea@yahoo.com>
2022-10-19 23:43:16 +0100

    switch ternary around

--- src/pagini/simulariPage.js
+++ src/pagini/simulariPage.js
@@ -250,7 +250,7 @@ export default function SimulariPage() {
                                                     </Button>
                                                 </div>
                                                 <div className={classes.centerDiv}>
-                                                    <Link color="secondary" href={rezultate ? new Date(simulare.Simulare.StartDate).getTime() < Date.now().getTime() : "#"} download className={classes.centerDiv}>
+                                                    <Link color="secondary" href={new Date(simulare.Simulare.StartDate).getTime() < Date.now().getTime() ?  rezultate : "#"} download className={classes.centerDiv}>
                                                         Rezultate simulare
                                                     </Link>
                                                 </div>
